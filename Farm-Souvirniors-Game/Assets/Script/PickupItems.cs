a0d877aa86f20079a3c49b5c8aebbac0559c0491
wasitphon kaewpoolsri <63654285+sokmok000@users.noreply.github.com>
2022-02-03 03:39:32 +0700

    func : havest
    func : havest

---
+++ Farm-Souvirniors-Game/Assets/Script/PickupItems.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PickupItems : MonoBehaviour
+{ 
+  //  private void Update() {
+  //  if (Input.GetKey(KeyCode.Space))
+  //       {
+  //           Debug.Log("space key was pressed");
+  //       }
+  // }
+        bool havest = false;
+        bool clash = false;
+   private void OnTriggerEnter2D(Collider2D other) {
+      if(other.tag == "Player"){
+      clash = true;
+      }
+   }
+       void OnHavest (InputValue value){
+         if(clash){
+          Debug.Log("test2");
+          Destroy(gameObject);
+         }
+      
+      }
+     
+  }
+
